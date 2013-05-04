3c41bdc65d6a43a44f6e6a0a303e4ebc6017213f
roryk <rory.kirchner@gmail.com>
2013-05-04 12:10:40 -0400

    Pass map to main instead of the view.

--- scripts/run_idr.py
+++ scripts/run_idr.py
@@ -1,25 +1,22 @@
 # script to run analysis
 from argparse import ArgumentParser
-from cluster_helper import cluster_view
+from cluster_helper.cluster import cluster_view
 from spp_idr import idr
 import os
 
-
-def main(control, experimental, tool_path, view=None):
-    if not view:
-        mapper = map
+def main(control, experimental, tool_path, mapper):
     spp_path = os.path.join(tool_path, "phantompeakqualtools", "run_spp.R")
     idr_runner_path = os.path.join(tool_path, "idrCode", "batch-consistency-analysis.r")
     idr_plotter_path = os.path.join(tool_path, "idrCode", "batch-consistency-plot.r")
     peak_caller = idr.SPPPeakCaller(spp_path)
-    peak_caller.set_map_function(view.map)
+    peak_caller.set_map_function(mapper)
     peaks = idr.run_analysis(control, experimental, spp_path, idr_runner_path,
                              idr_plotter_path, mapper)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(description='run IDR analysis on a set of chip-seq calls')
-    parser.add_argument('--controls', nargs="+", help="Control BAM files")
+    parser.add_argument('--control', nargs="+", help="Control BAM files")
     parser.add_argument('--experimental', nargs="+", help="Experimental BAM files")
     parser.add_argument('--lsf-queue', help="LSF queue name")
     parser.add_argument('--sge-queue', help="SGE queue name")
@@ -30,12 +27,12 @@ if __name__ == "__main__":
 
     if args.lsf_queue:
         with cluster_view("LSF", args.lsf_queue, args.num_jobs) as view:
-            main(args.control, args.experimental, args.spp_path, view)
+            main(args.control, args.experimental, args.tool_path, view.map)
     elif args.sge_queue:
         with cluster_view("LSF", args.lsf_queue, args.num_jobs) as view:
-            main(args.control, args.experimental, args.spp_path, view)
+            main(args.control, args.experimental, args.tool_path, view.map)
     elif args.torque_queue:
         with cluster_view("LSF", args.lsf_queue, args.num_jobs) as view:
-            main(args.control, args.experimental, args.spp_path, view)
+            main(args.control, args.experimental, args.tool_path, view.map)
     else:
-        main(args.control, args.experimental, args.spp_path, None)
+        main(args.control, args.experimental, args.tool_path, map)
